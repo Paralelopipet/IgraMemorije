93956fb5cf248fa378974204d7de37561da737e4
paralelopipet <damjan.denic@gmail.com>
2017-10-21 11:41:33 +0200

    radili smo nesto, wow

--- app/src/main/java/rs/systempro/igramemorije/GameActivity.java
+++ app/src/main/java/rs/systempro/igramemorije/GameActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -27,7 +28,10 @@ public class GameActivity extends AppCompatActivity {
     int width;
     int h;
     int w;
+    int solved=0;
     ArrayList<Drawable> images;
+    ArrayList<ImageButton> lista = new ArrayList<ImageButton>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,9 +69,12 @@ public class GameActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), height+" "+width, Toast.LENGTH_SHORT).show();
 
                 for (int i = 0; i < h * w; i++) {
-                    ImageButton b = new ImageButton(getApplicationContext());
+                    final int image=i;
+                    final ImageButton b = new ImageButton(getApplicationContext());
                     b.setBackgroundColor(Color.RED);
-                    b.setImageDrawable(images.get(i));
+                    //b.setImageDrawable(images.get(i));
+                    b.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    b.setPadding(0,0,0,0);
                     glMreza.addView(b);
                     GridLayout.LayoutParams param = new GridLayout.LayoutParams();
                     if (height/h < width/w) {
@@ -82,6 +89,63 @@ public class GameActivity extends AppCompatActivity {
 
                         param.setMargins(10,10,10,10);
                     b.setLayoutParams(param);
+                    b.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if(lista.contains(b))
+                                return;
+
+
+
+
+                            if(lista.size()==1)
+                            {
+                                b.setImageDrawable(images.get(image));
+                                lista.add(b);
+                                if(lista.get(0).getDrawable().getConstantState().equals(lista.get(1).getDrawable().getConstantState()))
+                                {
+                                    solved+=2;
+                                    Toast.makeText(GameActivity.this, solved + " " + (h*w), Toast.LENGTH_SHORT);
+                                    for(ImageButton but : lista)
+                                    {
+                                        but.setClickable(false);
+                                    }
+                                    lista.clear();
+                                    if(solved == h*w) {
+                                        Toast.makeText(GameActivity.this, "Reseno", Toast.LENGTH_SHORT);
+
+                                        finish();
+                                    }
+
+                                }
+                            }
+                            else
+
+
+
+
+                            if(lista.size()==2)
+                            {
+
+                                    lista.get(0).setImageDrawable(null);
+                                    lista.get(1).setImageDrawable(null);
+                                    lista.clear();
+
+                                b.setImageDrawable(images.get(image));
+                                lista.add(b);
+                            }else
+                            if(lista.size()==0)
+                            {
+                                b.setImageDrawable(images.get(image));
+                                lista.add(b);
+
+                            }
+
+
+                        }
+
+                    });
+
                 }
             }
 
